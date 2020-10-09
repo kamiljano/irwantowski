372b211b53d37fff02a4137a919828cb6a7c2f0a
Kamil Janowski <kgjanowski@gmail.com>
2020-10-09 15:24:35 +0300

    initial customized clock
    based on a template: https://colorlib.com/thank-you-for-downloading/?dlm-dp-dl=354
    
    Template demo: https://colorlib.com/etc/cs/comingsoon_04/index.html

---
+++ vendor/countdowntime/countdowntime.js
@@ -0,0 +1,48 @@
+(function ($) {
+    "use strict";
+
+    $.fn.extend({ 
+
+      count: function(options) {
+        if (!options.startTime) {
+          throw new Error('No startTime specified');
+        }
+
+        return this.each(function() {
+          const obj = $(this);
+          
+          initializeClock();
+
+          function getTimeFromStartTime() {
+            const now = moment();
+            return {
+              days: now.diff(options.startTime, 'days'),
+              hours: now.diff(options.startTime, 'hours') % 24,
+              minutes: now.diff(options.startTime, 'minutes') % 60,
+              seconds: now.diff(options.startTime, 'seconds') % 60
+            };
+          }
+
+          function initializeClock() {
+            const daysSpan = $(obj).find('.days');
+            const hoursSpan = $(obj).find('.hours');
+            const minutesSpan = $(obj).find('.minutes');
+            const secondsSpan = $(obj).find('.seconds');
+
+            function updateClock() { 
+              const t = getTimeFromStartTime();
+
+              daysSpan.html(t.days);
+              hoursSpan.html(('0' + t.hours).slice(-2));
+              minutesSpan.html(('0' + t.minutes).slice(-2));
+              secondsSpan.html(('0' + t.seconds).slice(-2))
+            }
+
+            updateClock();
+            setInterval(updateClock, 1000);
+          }
+        });
+      }
+    });
+
+})(jQuery);
